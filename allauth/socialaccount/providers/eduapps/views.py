0fedd369eae370492ba4a29a68926c5d23ae5763
Tobias Goecke <tobias.goecke@supratix.com>
2020-07-08 18:05:12 +0200

    eduapps prod
    Signed-off-by: Tobias Goecke <tobias.goecke@supratix.com>

--- allauth/socialaccount/providers/eduapps/views.py
+++ allauth/socialaccount/providers/eduapps/views.py
@@ -11,8 +11,8 @@ from .provider import EduAppsProvider
 
 class EduAppsOAuth2Adapter(OAuth2Adapter):
     provider_id = EduAppsProvider.id
-    access_token_url = 'https://demoapp.eduapps.de/oauth/token'
-    authorize_url = 'https://demoapp.eduapps.de/oauth/authorize'
+    access_token_url = 'https://login.eduapps.de/oauth/token'
+    authorize_url = 'https://login.eduapps.de/oauth/authorize'
 
     def complete_login(self, request, app, token, **kwargs):
         headers = {'Authorization': 'Bearer {0}'.format(token.token)}
