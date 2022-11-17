5492959069a3453c28172bef1501d0b7d09c8db3
Tobias Göcke <tobias.goecke@supratix.com>
2022-11-17 14:52:56 +0100

    up
    Signed-off-by: Tobias Göcke <tobias.goecke@supratix.com>

---
+++ allauth/socialaccount/providers/dfb/provider.py
@@ -0,0 +1,23 @@
+from allauth.socialaccount.providers.base import ProviderAccount
+from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
+
+
+class NoaAccount(ProviderAccount):
+    """Noa Account"""
+
+    pass
+
+
+class NoaProvider(OAuth2Provider):
+    """Provider for Noa"""
+
+    id = 'noa'
+    name = 'Noa'
+    account_class = NoaAccount
+
+    def extract_uid(self, data):
+
+        return str(data['preferred_username'])
+
+
+provider_classes = [NoaProvider]
